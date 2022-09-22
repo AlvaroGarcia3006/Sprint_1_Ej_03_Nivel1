41c25d4b49453e38d4474888ec05d6fe38945d87
AlvaroGarcia3006 <113432141+AlvaroGarcia3006@users.noreply.github.com>
2022-09-22 16:16:00 +0200

    alpha

--- src/nivel1_ej03/Main.java
+++ src/nivel1_ej03/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.Random;
 
 public class Main {
@@ -13,9 +12,8 @@ public class Main {
 		
 		String txtInput = "C:/Users/alvar/eclipse-workspace/Sp1_ej03_GarciaAlvaro/src/nivel1_ej03/Countries.txt";
 		String user = createUser();
-		//readCountries(txtInput);
-		randomCountry(user, readCountries(txtInput));
-		
+		int score = randomCountry(user, readCountries(txtInput));
+		ranking(user, score);
 	}
 	public static String createUser() {
 		
@@ -39,28 +37,35 @@ public class Main {
 		}
 		return hmCountries;
 	}
-	public static void randomCountry(String user, HashMap<String, String> hmCountries) {
+	public static int randomCountry(String user, HashMap<String, String> hmCountries) {
 		int points=0;
-		String answer, rKey, country;
+		String answer, rKey, capital;
 		Scanner sc = new Scanner(System.in);
 		Random r = new Random();
 		ArrayList<String> keys = new ArrayList<String>(hmCountries.keySet());
 		
-		System.out.println("Bienveni@ "+user+", empecemos a jugar:");
+		System.out.println("Bienvenid@ "+user+", empecemos a jugar:");
 		
-		for(int i=0; i<2; i++) {
+		for(int i=0; i<10; i++) {
 			rKey = keys.get(r.nextInt(keys.size()));
-			country = hmCountries.get(rKey);
-			System.out.print("Capital de "+country+": ");
+			capital = hmCountries.get(rKey);
+			System.out.print("Capital de "+rKey+": ");
 			answer = sc.nextLine();
-			for(Entry<String, String> entry : hmCountries.entrySet()) {
-				if(entry.getKey() == country) {
-					if(answer == entry.getValue()) {
-						points += 1;
-					}
-				}
+			if(answer.equalsIgnoreCase(capital)) {
+				points +=1;
 			}
 		}
-		System.out.println(points);
+		return points;
+	}
+	public static void ranking(String user, int score) {
+		String scoreboard = "C:/Users/alvar/eclipse-workspace/Sp1_ej03_GarciaAlvaro/src/nivel1_ej03/Clasificacion.txt";
+		try {
+			PrintWriter w = new PrintWriter(scoreboard);
+			w.println(user+": "+score);
+			w.close();
+			System.out.println("La clasificación está disponible.");	
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
